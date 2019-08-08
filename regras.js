3daf8ec4793693964ffde9cf87fe14e4f7294adc
MIQUEIAS DE ABREU MARTINS <miqueias.amartins@senacsp.edu.br>
2019-08-08 20:38:01 -0300

    Conhecendo regras

---
+++ regras.js
@@ -0,0 +1,34 @@
+// Exemplo para não se fazer
+// let 1nome = '';
+
+// Exemplo para se fazer
+// string
+let nome = `vinicius`;
+
+//Tipo inteiro
+let idade = 20
+
+// Tipo Decimal
+let peso = 55.25
+
+// Tipo boleano
+let visualizado = true;
+
+// criar constante
+const nome1 = 'vinicius';
+
+// Exibir informações 
+console.log('Este éo console .log');
+
+console.log('Exibindo variáveis');
+console.log('Nome: ' + nome);
+nome = 'José';
+console.log('Nome: ' + nome);
+nome2 = 'Roberto';
+console.log('Nome const: ' + nome2)
+
+console.log('Nome: ' + nome + ' idade: '+ idade);
+console.log(`Nome: ${nome} idade ${idade}`);
+
+
+
