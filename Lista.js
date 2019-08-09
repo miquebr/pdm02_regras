93e36594f9e168e85b89cdacc7ef5714bc91b3c2
MIQUEIAS DE ABREU MARTINS <miqueias.amartins@senacsp.edu.br>
2019-08-08 22:10:16 -0300

    Lista Arrey

---
+++ Lista.js
@@ -0,0 +1,21 @@
+// Antigamente
+var listaantiga = Array();
+
+// Novo
+let lista = [];
+console.log(lista);
+
+// Adicionando um ítem
+lista.push(`Carvão`);
+lista.push(`Carne`);
+lista.push(`Cerveja sem alcool`);
+lista.push(10);
+console.log(lista);
+
+
+// Que todo array começa com zero;
+console.log(lista[0]);
+
+// O pop sempre remove o ultimo ítem da lista
+console.log(lista.pop())
+console.log(lista);
