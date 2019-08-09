4310d304ed63483b70e5e2e6c0ca3a4be13c5af9
MIQUEIAS DE ABREU MARTINS <miqueias.amartins@senacsp.edu.br>
2019-08-09 20:25:53 -0300

    Lista Array

--- Lista.js
+++ Lista.js
@@ -19,3 +19,31 @@ console.log(lista[0]);
 // O pop sempre remove o ultimo ítem da lista
 console.log(lista.pop())
 console.log(lista);
+console.log(lista.splice(1,0,'Alcool'));
+// primeiro parametro - começa
+// segundo parametro - deleta itens
+// terceiro parametro adiciona itens
+console.log(lista.splice(0,1));
+
+// adicionar item em posição específica
+// utilizando filter
+// filtrando informações
+//console.log(lista.splice0,0''elemento''));
+console.log(lista.filter(e => e === 'Cerveja'));
+
+// Buscando informação com o find
+console.log('Utilizando Find');
+console.log(lista.find(v => v === 'Carne'));
+
+
+// Verifivar tamanho da lista
+console.log('Tamanho da lista');
+console.log(lista.length)
+
+console.log(lista);
+
+
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+const resultado = numeros.map(Math.sqrt)
+console.log('Utilizando Raiz queadrada')
+console.log(resultado)
